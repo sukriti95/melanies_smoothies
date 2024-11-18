cc51ea5dd89d1d05cd08382a4d54d61f8cd4bbb4
Sukriti Ranjan <56404744+sukriti95@users.noreply.github.com>
2024-11-18 16:35:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 import requests
-import Pandas as pd
+import pandas as pd
 
 
 
